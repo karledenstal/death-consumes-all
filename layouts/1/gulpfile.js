aed45659cdc79ad447d5f0dff35f75474ba6ae2a
Karl Edenstål <karl.edenstal@rhyme.se>
2022-02-24 15:37:26 +0100

    fix env

--- layouts/1/gulpfile.js
+++ layouts/1/gulpfile.js
@@ -1,86 +1,62 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const csso = require('gulp-csso');
-const plumber = require('gulp-plumber');
-const notify = require('gulp-notify');
-const jshint = require('gulp-jshint');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
-const autoprefixer = require('gulp-autoprefixer');
-const sync = require('browser-sync').create();
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const sync = require("browser-sync").create();
+const terser = require("gulp-terser");
+const urlAdjuster = require("gulp-css-url-adjuster");
+const minify = require("gulp-minify-css");
+const babel = require("gulp-babel");
 
-gulp.task('sync', () => {
-  sync.init({
-    port: 5001,
-    server: {
-      baseDir: './',
-    },
-    startPath: 'index.html',
-  });
-});
-
-gulp.task('sass', () => {
+function scss() {
   return gulp
-    .src('./lib/styles/**')
-    .pipe(plumber({ errorHandler: (err) => {
-      notify.onError({
-        title: 'Gulp error in ' + err.plugin,
-        message: err.toString(),
-      })(err);
-    }}))
+    .src("src/styles/**/*.scss", { sourcemaps: true })
     .pipe(sass())
-    .pipe(autoprefixer())
-    .pipe(csso())
-    .pipe(gulp.dest('./dist/css'))
     .pipe(
-      sync.reload({
-        stream: true,
+      urlAdjuster({
+        prepend: "../src/",
       })
-    );
-});
+    )
+    .pipe(postcss([autoprefixer()]))
+    .pipe(minify({ processImport: false }))
+    .pipe(gulp.dest("public", { sourcemaps: "." }));
+}
 
-gulp.task('sass-pages', () => {
+function js() {
   return gulp
-    .src('./lib/styles/pages/**')
-    .pipe(plumber({ errorHandler: (err) => {
-      notify.onError({
-        title: 'Gulp error in ' + err.plugin,
-        message: err.toString(),
-      })(err);
-    }}))
-    .pipe(sass({
-      outputStyle: 'nested',
-      precision: 10,
-    }))
-    .pipe(autoprefixer())
-    .pipe(csso())
-    .pipe(gulp.dest('./dist/css/pages'))
+    .src("src/js/**/*.js", { sourcemaps: true })
     .pipe(
-      sync.reload({
-        stream: true,
+      babel({
+        plugins: [
+          "@babel/plugin-transform-modules-commonjs"
+        ],
       })
-    );
-});
+    )
+    .pipe(terser())
+    .pipe(gulp.dest("public", { sourcemaps: "." }));
+}
 
-gulp.task('js', () => {
-  return gulp
-  .src('./lib/scripts/*.js')
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('./dist/js'))
-  .pipe(sync.reload({ stream: true }));
-});
+function browsersyncServe(cb) {
+  sync.init({
+    server: {
+      port: 5001,
+      baseDir: ".",
+    },
+  });
+  cb();
+}
 
-gulp.task('lint', () => {
-  return gulp
-  .src('./lib/scripts/*.js')
-  .pipe(jshint())
-  .pipe(jshint.reporter('jshint-stylish'))
-});
+function browsersyncReload(cb) {
+  sync.reload();
+  cb();
+}
 
-gulp.task('default', ['sync', 'sass', 'lint'], () => {
-  gulp.watch('./lib/styles/**', ['sass']);
-  gulp.watch('./lib/styles/pages', ['sass-pages']);
-  gulp.watch('*.html', sync.reload);
-  gulp.watch('./lib/pages/*.html', sync.reload);
-});
+function watch() {
+  gulp.watch(["*.html", "src/**/*.html"], browsersyncReload);
+  gulp.watch(
+    ["src/**/*.scss", "src/**/*.js"],
+    gulp.series(scss, js, browsersyncReload)
+  );
+}
+
+exports.default = gulp.series(gulp.parallel(scss, js), browsersyncServe, watch);
---
+++ layouts/1/src/js/main.js
@@ -0,0 +1,93 @@
+import gulp from "gulp";
+
+$(document.body).ready(() => {
+  if (document.title.match(/->/i)) {
+    document.title = document.title.split(" -> ")[1].toLowerCase();
+  }
+});
+
+$(function () {
+  $(window).scroll(() => {
+    let nav = $("#userpanels");
+    if ($(this).scrollTop() > 850) {
+      nav.addClass("show");
+    } else {
+      nav.removeClass("show");
+    }
+  });
+});
+
+$(function () {
+  $("[title]").style_my_tooltips({
+    tip_follows_cursor: true,
+    tip_delay_time: 0,
+    tip_fade_speed: 250,
+  });
+});
+
+// Post Autosaver by Black
+// http://code.jfbs.net/?showtopic=81
+$(document).ready(function () {
+  if (
+    window.location.href.indexOf("act=Post") !== -1 &&
+    window.location.href.indexOf("&f=") !== -1 &&
+    window.location.href.indexOf("&CODE=08") == -1
+  ) {
+    var forumParameters = (function (url) {
+      var result = {};
+      for (var i = 0; i < url.length; i++) {
+        var parameter = url[i].split("=");
+        if (parameter.length != 2) continue;
+        result[parameter[0]] = decodeURIComponent(
+          parameter[1].replace(/\+/g, " ")
+        );
+      }
+      return result;
+    })(window.location.search.substr(1).split("&"));
+    var subDomain = location.hostname.split(".").shift();
+    function savePost() {
+      if (forumParameters["t"]) {
+        localStorage[subDomain + forumParameters["f"] + forumParameters["t"]] =
+          $("textarea[name='Post']").val();
+      } else {
+        localStorage[subDomain + forumParameters["f"]] = $(
+          "textarea[name='Post']"
+        ).val();
+      }
+    }
+    function loadPost() {
+      if (forumParameters["t"]) {
+        if (forumParameters["p"]) {
+          return false;
+        }
+        $("textarea[name='Post']").val(
+          localStorage[subDomain + forumParameters["f"] + forumParameters["t"]]
+        );
+      } else {
+        $("textarea[name='Post']").val(
+          localStorage[subDomain + forumParameters["f"]]
+        );
+      }
+    }
+    function savePostRepeat() {
+      setTimeout(function () {
+        savePost();
+        savePostRepeat();
+      }, 30000);
+    }
+    $(window).unload(function () {
+      savePost();
+    });
+    $("form[name='REPLIER']").on("submit", function () {
+      if (forumParameters["t"]) {
+        localStorage.removeItem(
+          subDomain + forumParameters["f"] + forumParameters["t"]
+        );
+      } else {
+        localStorage.removeItem(subDomain + forumParameters["f"]);
+      }
+    });
+    loadPost();
+    savePostRepeat();
+  }
+});
